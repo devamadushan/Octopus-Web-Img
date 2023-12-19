30e2294302692f65746e839f7263bc431842910f
Deva <deva.thevaranchan@gmail.com>
2023-12-19 11:48:31 +0100

    api

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort
+import random
 
 '''
--pip insall falsk
+-pip install falsk
 -pip install flask_restful
 '''
 
 app = Flask(__name__)
 api = Api(app)
 ecolabs = {
-    1: {"temperature": 27, "humidity": 90, "pressure": 3},
-    2: {"temperature": 27, "humidity": 90, "pressure": 3},
-    3: {"temperature": 27, "humidity": 90, "pressure": 3},
-    4: {"temperature": 27, "humidity": 90, "pressure": 3},
-    5: {"temperature": 27, "humidity": 90, "pressure": 3}
+    1: {"temperature": round(random.uniform(-10, 45), 0), "humidity": round(random.uniform(40, 70), 0),
+        "pressure": round(random.uniform(980, 1020), 0)},
+    2: {"temperature": round(random.uniform(-10, 45), 0), "humidity": round(random.uniform(40, 70), 0), "pressure": round(random.uniform(980, 1020), 0)},
+    3: {"temperature": round(random.uniform(-10, 45), 0), "humidity": round(random.uniform(40, 70), 0), "pressure": round(random.uniform(980, 1020), 0)},
+    4: {"temperature": round(random.uniform(-10, 45), 0), "humidity": round(random.uniform(40, 70), 0), "pressure": round(random.uniform(980, 1020), 0)},
+    5: {"temperature": round(random.uniform(-10, 45), 0), "humidity": round(random.uniform(40, 70), 0), "pressure": round(random.uniform(980, 1020), 0)}
 }
 
 
@@ -21,10 +23,12 @@ class Ecolabs(Resource):
     def get(self):
         return ecolabs
 
+class Ecolab(Resource):
+    def get(self, id):
+        return ecolabs[id]
 
-api.add_resource(Ecolabs, '/chp')
 
-
-
+api.add_resource(Ecolabs, '/ecolabs')
+api.add_resource(Ecolab, '/ecolab/<int:id>')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="10.118.10.29",port=8080,debug=True)
