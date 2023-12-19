fa414bcaba189b2dd5fb10f003901e5d52f7482a
Deva <deva.thevaranchan@gmail.com>
2023-12-19 13:55:20 +0100

    templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api, reqparse, abort
 import random
 
@@ -10,12 +10,21 @@ import random
 app = Flask(__name__)
 api = Api(app)
 ecolabs = {
-    1: {"temperature": round(random.uniform(-10, 45), 0), "humidity": round(random.uniform(40, 70), 0),
+    1: {"Name": "ecolab 1", "temperature": round(random.uniform(-10, 45), 0),
+        "humidity": round(random.uniform(40, 70), 0),
         "pressure": round(random.uniform(980, 1020), 0)},
-    2: {"temperature": round(random.uniform(-10, 45), 0), "humidity": round(random.uniform(40, 70), 0), "pressure": round(random.uniform(980, 1020), 0)},
-    3: {"temperature": round(random.uniform(-10, 45), 0), "humidity": round(random.uniform(40, 70), 0), "pressure": round(random.uniform(980, 1020), 0)},
-    4: {"temperature": round(random.uniform(-10, 45), 0), "humidity": round(random.uniform(40, 70), 0), "pressure": round(random.uniform(980, 1020), 0)},
-    5: {"temperature": round(random.uniform(-10, 45), 0), "humidity": round(random.uniform(40, 70), 0), "pressure": round(random.uniform(980, 1020), 0)}
+    2: {"Name": "ecolab 2", "temperature": round(random.uniform(-10, 45), 0),
+        "humidity": round(random.uniform(40, 70), 0),
+        "pressure": round(random.uniform(980, 1020), 0)},
+    3: {"Name": "ecolab 3", "temperature": round(random.uniform(-10, 45), 0),
+        "humidity": round(random.uniform(40, 70), 0),
+        "pressure": round(random.uniform(980, 1020), 0)},
+    4: {"Name": "ecolab 4", "temperature": round(random.uniform(-10, 45), 0),
+        "humidity": round(random.uniform(40, 70), 0),
+        "pressure": round(random.uniform(980, 1020), 0)},
+    5: {"Name": "ecolab 5", "temperature": round(random.uniform(-10, 45), 0),
+        "humidity": round(random.uniform(40, 70), 0),
+        "pressure": round(random.uniform(980, 1020), 0)}
 }
 
 
@@ -23,12 +32,23 @@ class Ecolabs(Resource):
     def get(self):
         return ecolabs
 
+
 class Ecolab(Resource):
     def get(self, id):
         return ecolabs[id]
 
 
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', ecolabs=ecolabs)
+
+
 api.add_resource(Ecolabs, '/ecolabs')
 api.add_resource(Ecolab, '/ecolab/<int:id>')
+
+
 if __name__ == '__main__':
-    app.run(host="10.118.10.29",port=8080,debug=True)
+    app.run(host="10.118.10.29", port=8080, debug=True)
